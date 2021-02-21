9a3819504dfa672c4dd989690b61e68857b3653a
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2021-02-21 20:00:31 -0300

    add schema check and bean definition

--- web/src/main/java/com/freemanpivo/chassi/web/TemplateApplicationConfiguration.java
+++ web/src/main/java/com/freemanpivo/chassi/web/TemplateApplicationConfiguration.java
@@ -1,5 +1,6 @@
 package com.freemanpivo.chassi.web;
 
+import com.freemanpivo.chassi.domain.port.RetrieveUserStored;
 import com.freemanpivo.chassi.domain.usecase.GetUserDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +9,8 @@ import org.springframework.context.annotation.Configuration;
 public class TemplateApplicationConfiguration {
 
     @Bean
-    GetUserDetails getUserDetails() {
-        return new GetUserDetails();
+    GetUserDetails getUserDetails(RetrieveUserStored retrieveUserStored) {
+        return new GetUserDetails(retrieveUserStored);
     }
 
 }
